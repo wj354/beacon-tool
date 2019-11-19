76dca7989dd9e2b8bbd6712946e78784adb9ea72
wj008 <26029682@qq.com>
2019-11-19 14:33:25 +0800

    修复属性下划线转换的bug

--- lib/MakeForm.php
+++ lib/MakeForm.php
@@ -184,8 +184,8 @@ class MakeForm
         if (!empty($field['boxAttrs'])) {
             $boxAttrs = Helper::convertArray($field['boxAttrs'], []);
             foreach ($boxAttrs as $item) {
-                $name = lcfirst(Utils::attrToCamel($item['name']));
-                $field['box-' . $name] = $item['value'];
+                $name = ucfirst(Utils::attrToCamel($item['name']));
+                $field['box' . $name] = $item['value'];
             }
             unset($field['boxAttrs']);
         }
