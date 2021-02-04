6f1e7ef6156dc0104ea8fe5fdd987ebc669836ff
Sphynx <327388905@qq.com>
2021-02-04 18:33:41 +0800

    修复数据库连接问题

--- lib/ToolDb.php
+++ lib/ToolDb.php
@@ -24,13 +24,13 @@ class ToolDb
         if (!$row) {
             throw new \Exception("获取数据库信息失败");
         }
-        $host = empty($row['db_host']) ? Config::get("db_host") : $row['db_host'];
-        $port = empty($row['db_port']) ? Config::get("db_port") : $row['db_port'];
-        $name = empty($row['db_name']) ? Config::get("db_name") : $row['db_name'];
-        $user = empty($row['db_user']) ? Config::get("db_user") : $row['db_user'];
-        $pass = empty($row['db_pwd']) ? Config::get("db_pwd") : $row['db_pwd'];
-        $prefix = empty($row['db_prefix']) ? Config::get("db_prefix") : $row['db_prefix'];
-        $charset = empty($row['db_charset']) ? Config::get("db_charset") : $row['db_charset'];
+        $host = empty($row['db_host']) ? Config::get("db.db_host") : $row['db_host'];
+        $port = empty($row['db_port']) ? Config::get("db.db_port") : $row['db_port'];
+        $name = empty($row['db_name']) ? Config::get("db.db_name") : $row['db_name'];
+        $user = empty($row['db_user']) ? Config::get("db.db_user") : $row['db_user'];
+        $pass = empty($row['db_pwd']) ? Config::get("db.db_pwd") : $row['db_pwd'];
+        $prefix = empty($row['db_prefix']) ? Config::get("db.db_prefix") : $row['db_prefix'];
+        $charset = empty($row['db_charset']) ? Config::get("db.db_charset") : $row['db_charset'];
         if (empty($charset)) {
             $charset = 'utf8';
         }
